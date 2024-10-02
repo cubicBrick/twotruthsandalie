40c261b18d3871236ce6fa4cbf01ee51e64dc662
JaydenHor <jaydenhor@outlook.com>
2024-10-01 17:09:36 -0700

    Added host controls

--- api/index.py
+++ api/index.py
@@ -14,6 +14,15 @@ DEFAULTCHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
 LOG_FILE = "./.log"
 random.seed(os.urandom(32))
 
+HTTP_BAD_REQUEST = 400
+HTTP_UNOUTHORIZED = 401
+HTTP_FORBIDDEN = 403
+HTTP_NOT_FOUND = 404
+HTTP_METHOD_NOT_ALLOWED = 405
+HTTP_NOT_ACCEPTABLE = 406
+HTTP_SERVER_ERROR = 500
+HTTP_OK = 200
+HTTP_CREATED = 201
 
 # Logs the data with a timestamp
 def log(data: str, *, file=LOG_FILE) -> None:
@@ -51,10 +60,11 @@ def pageTruthsLiesJoin():
             username = data.get("username")
             log(f"Attempted to join game with id [{gameid}], username [{username}].")
             if gameid not in twotruthsandaliegames.keys():
-                return jsonify({"error": "Game not found"}), 404
+                return jsonify({"error": "Game not found"}), HTTP_NOT_FOUND
             playerid = randString(32)
             twotruthsandaliegames[gameid].players[playerid] = username
-            return jsonify({"good": "Game found", "userid":playerid}), 200
+            twotruthsandaliegames[gameid].played[playerid] = False
+            return jsonify({"good": "Game found", "userid":playerid}), HTTP_CREATED
         elif data.get("type") == "play":
             t1 = data.get('t1')
             t2 = data.get('t2')
@@ -62,16 +72,17 @@ def pageTruthsLiesJoin():
             playerid = data.get('id')
             gameid = data.get('gameid')
             if gameid not in twotruthsandaliegames.keys():
-                return jsonify({"error": "Game not found"}), 404
+                return jsonify({"error": "Game not found"}), HTTP_NOT_FOUND
             if playerid not in twotruthsandaliegames[gameid].players.keys():
-                return jsonify({'error': 'Player not found'}), 404
-            if playerid in twotruthsandaliegames[gameid].tandl.keys():
-                return jsonify({'error': "Already played"}), 403
+                return jsonify({'error': 'Player not found'}), HTTP_NOT_FOUND
+            if twotruthsandaliegames[gameid].played[playerid]:
+                return jsonify({'error': "Already played"}), HTTP_FORBIDDEN
+            if not twotruthsandaliegames[gameid].submitting:
+                return jsonify({'error', 'Submitting phase closed'}), HTTP_FORBIDDEN
+            twotruthsandaliegames[gameid].played[playerid] = True
             twotruthsandaliegames[gameid].newPlayer(t1, t2, l1, playerid)
-            return jsonify({'good' : "Added truth and lies"}), 200
-            
-
-
+            return jsonify({'good' : "Added truth and lies"}), HTTP_OK
+        
     return render_template("/truthandlie/join.html")
 
 
@@ -79,6 +90,8 @@ class truthAndLie:
     players: "dict[str, str]" = {}
     played: "dict[str, bool]" = {}
     tandl: "dict[str, tuple[str, str, str]]" = {}
+    host: "str" = ""
+    submitting = True
     currtime = datetime.now()
 
     def newPlayer(self, t1: str, t2: str, l1: str, id: str):
@@ -89,12 +102,28 @@ class truthAndLie:
 def pageHostTruthLies():
     if request.method == "GET":
         return render_template("/truthandlie/host.html")
-    else:
+    elif request.method == "POST":
         data = request.json
-        id = data.get("id")
-        log(f"New game started with id: [{id}]")
-        twotruthsandaliegames[id] = truthAndLie()
-        return jsonify({})
+        if(data.get("type") == "newid"):
+            id = randString(10)
+            yourid = randString(16)
+            log(f"New game started with id: [{id}] from player id [{yourid}]")
+            twotruthsandaliegames[id] = truthAndLie()
+            twotruthsandaliegames[id].host = yourid
+            return jsonify({'id' : id, 'yourid': yourid}), HTTP_CREATED
+        elif(data.get("type") == "refresh"):
+            gameid = data.get("gameid")
+            if gameid not in twotruthsandaliegames.keys():
+                return jsonify({"error": "Game not found"}), HTTP_NOT_FOUND
+            playerid = data.get("playerid")
+            if not playerid == twotruthsandaliegames[gameid].host:
+                return jsonify({"error": "player is not host"}), HTTP_FORBIDDEN
+            res:"list[list[str, bool]]" = []
+            for i in twotruthsandaliegames[gameid].players.keys():
+                res.append([twotruthsandaliegames[gameid].players[i],
+                            twotruthsandaliegames[gameid].played[i]])
+            return jsonify({'players': res}), HTTP_OK
+            
 
 
 if __name__ == "__main__":
