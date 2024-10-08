c11784ad27b5ff5225f460db2ed95c35560c4c18
JaydenHor <jaydenhor@outlook.com>
2024-10-08 14:07:16 -0700

    Fixed check page

--- api/static/scripts/thingsnottodo/suggust.js
+++ api/static/scripts/thingsnottodo/suggust.js
@@ -1,10 +1,16 @@
 async function go(){
-    var sugguestion = document.getElementById("what").value;
-    const response = await fetch('/thingsnottodo/sugguest', {
+    var sugguestion = document.getElementById("id").value;
+    const response = await fetch('/thingsnottodo/suggest', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({"sugguestion":sugguestion})
     });
     const data = await response.json();
-    document.getElementById("id").innerHTML = "Your sugguestion ID: " + data.id + "<br><a href=\"/thingsnottodo/check?id=" + data.id + ">View it here</a>"
+    if(data.error){
+        alert("Error: " + data.error)
+        return;
+    }
+    document.getElementById("idview").innerHTML = "Your sugguestion ID: " + data.id;
+    document.getElementById("view").setAttribute("href", "/thingsnottodo/check?id=" + data.id);
+    document.getElementById("view").removeAttribute("hidden");
 }
