c74452de2aaeb43176c1c0497f64ff3b01d51ab1
JaydenHor <jaydenhor@outlook.com>
2024-10-17 17:26:31 -0700

    e

--- api/index.py
+++ api/index.py
@@ -129,7 +129,13 @@ def pageHome():
             return jsonify({"result" : "1"}), HTTP_OK
         else:
             return jsonify({"result": "0"}), HTTP_OK
-        
+
+
+######################################
+#       ^^^ Authentication ^^^       #
+#    vvv Two Truths and a Lie vvv    #
+######################################
+
 
 @app.route("/twotruthsandalie/home")
 def pageTruthsLiesHome():
@@ -218,6 +224,13 @@ def pageHostTruthLies():
                 return jsonify({"error": "Player is not host"}), HTTP_FORBIDDEN
             twotruthsandaliegames[gameid].submitting = False
             return jsonify({"good" : "Finished submitting"}), HTTP_OK            
+
+
+
+######################################
+#    ^^^ Two truths and a lie ^^^    #
+#      vvv Things not to do vvv      #
+######################################
 
 @app.route("/thingsnottodo", methods=["GET", "POST"])
 def pageThingsNotToDo():
